ea1cdb87e63a61aff14d5e4e4b79aa0fb1240418
Fumitaka Tokumitsu <3142849+toku345@users.noreply.github.com>
2023-12-30 15:26:35 +0900

    変数名をオリジナルに揃える

--- lib/color.rb
+++ lib/color.rb
@@ -7,12 +7,12 @@ extend T::Sig # rubocop:disable Style/MixinUsage
 
 Color = Vec3
 
-sig { params(input: IO, color: Color).void }
-def write_color(input, color)
+sig { params(input: IO, pixel_color: Color).void }
+def write_color(input, pixel_color)
   # Write the translated [0,255] value of each color component.
-  ir = (255.999 * color.x).to_i
-  ig = (255.999 * color.y).to_i
-  ib = (255.999 * color.z).to_i
+  ir = (255.999 * pixel_color.x).to_i
+  ig = (255.999 * pixel_color.y).to_i
+  ib = (255.999 * pixel_color.z).to_i
 
   input.puts "#{ir} #{ig} #{ib}"
 end
